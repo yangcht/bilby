Fix a missing factor of 2 in the Fisher Matrix proposals

bilby/bilby_mcmc/proposals.py
@@ -835,7 +835,7 @@ class FisherMatrixProposal(AdaptiveGaussianProposal):
             self.steps_since_update = 0
 
         jump = self.scale * np.random.multivariate_normal(
-            self.mean, self.iFIM, check_valid="ignore"
+            self.mean, 2 * self.iFIM, check_valid="ignore"
         )
 
         for key, val in zip(self.parameters, jump):

bilby/core/fisher.py
@@ -66,7 +66,7 @@ class FisherMatrixPosteriorEstimator(object):
 
         self.mean = np.array(list(sample.values()))
         self.iFIM = self.calculate_iFIM(sample)
-        return np.random.multivariate_normal(self.mean, self.iFIM, n)
+        return np.random.multivariate_normal(self.mean, 2 * self.iFIM, n)
 
     def sample_dataframe(self, sample, n=1):
         samples = self.sample_array(sample, n)