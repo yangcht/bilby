befa8c5f016ffe49b7ce7300fc7cb1b35f28dac9
Colm Talbot <colm.talbot@ligo.org>
2024-06-21 21:25:43 +0000

    MAINT: suppress dynesty warning

--- bilby/core/sampler/dynesty.py
+++ bilby/core/sampler/dynesty.py
@@ -677,10 +677,18 @@ class Dynesty(NestedSampler):
         chain of nested samples within dynesty and have to be removed before
         restarting the sampler.
         """
+
+        import warnings
         logger.debug("Running sampler with checkpointing")
 
         old_ncall = self.sampler.ncall
         sampler_kwargs = self.sampler_function_kwargs.copy()
+        warnings.filterwarnings(
+            "ignore",
+            message="The sampling was stopped short due to maxiter/maxcall limit*",
+            category=UserWarning,
+            module="dynesty.sampler",
+        )
         while True:
             self.finalize_sampler_kwargs(sampler_kwargs)
             self.sampler.run_nested(**sampler_kwargs)
